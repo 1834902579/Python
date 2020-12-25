93c1ba9edc27a3f19d222edb5f0702a61dc2cb88
Md-Masum <66910507+1834902579@users.noreply.github.com>
2020-12-24 22:53:32 -0800

    Update DicKey.py

--- Basic : 1/Dictionary/dict_keys/DicKey.py
+++ Basic : 1/Dictionary/dict_keys/DicKey.py
@@ -1,10 +1,11 @@
 #Dictionary Keywords
 
 dictionary = {
-  'a' : [1,2,3] , 
-  'b' : 'hello' ,
-  'x' : True ,
-  100 : [23,45] ,
+  'a' : [1,2,3], 
+  'b' : 'hello',
+  'x' : True,
+  100 : [23,45],
+  100 : [33,45],#same two key works bt show the upgraded value
   #[100] :34 ,#con't use because of Dict id imm
   '[100]':67
 }
